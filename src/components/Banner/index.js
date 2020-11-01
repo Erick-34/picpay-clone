07f76d9af0f07324f0664b668c026d44dc5e9109
Erick-34 <erickoliveira2060@gmail.com>
2020-11-01 08:07:52 -0300

    add: components and screens

---
+++ src/components/Banner/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Container, Details, Img, Title, Description } from "./styles";
+
+import img13 from "../../images/13.png";
+
+export default function Banner() {
+  return (
+    <Container>
+      <Details>
+        <Title>Cobre um amigo</Title>
+        <Description>
+          Mantenha suas parcerias em dia, use o PicPay para fazer uma cobrança
+        </Description>
+      </Details>
+      <Img source={img13} />
+    </Container>
+  );
+}
